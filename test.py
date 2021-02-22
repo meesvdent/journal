a0ae8360f59d0cb5ba2da553df00d972ed527e0f
meesvdent <meesvdent@gmail.com>
2021-02-22 20:30:59 +0100

    added theme detection

---
+++ test.py
@@ -0,0 +1,19 @@
+from helpers import open_file
+from themes import find_themes
+
+
+
+def test_open_file():
+    filename = "/Users/meesvdent/Journal/journal_entries/22_02_2021.md"
+    content = open_file(filename)
+    return(content)
+
+def test_find_themes():
+    content = test_open_file()
+    themes = find_themes(content)
+    print(themes)
+
+
+if __name__ == "__main__":
+    test_open_file()
+    test_find_themes()
---
+++ themes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+"""
+Puts the test from different subjects into different text files
+
+"""
+
+
+def find_themes(day_text):
+    """
+    Makes a list of themes in the day files.
+
+    """
+
+
+    themes = []
+    day_text = day_text.replace("\n", " ")
+    words = day_text.split(" ")
+    for i in range(0, len(words)):
+        print(i)
+        print("that"+words[i]+"this")
+        if words[i] == "##":
+            print("me")
+            themes.append(words[i+1])
+
+    return themes
+
+
+
+
